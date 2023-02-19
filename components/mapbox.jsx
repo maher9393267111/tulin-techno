6c4c457ad6d9a6c710e9662edfb08b7eec449218
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-02-19 16:17:46 +0300

    update map location

--- components/mapbox.jsx
+++ components/mapbox.jsx
@@ -3,10 +3,17 @@ import React from 'react'
 export default function mapbox() {
   return (
     <div className="map-canvas">
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d182586.0420340798!2d-73.99038430252834!3d40.749936548349346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1670824458615!5m2!1sen!2sbd"
-      loading="lazy"
-    ></iframe>
+
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48167.997665822826!2d28.918601990244273!3d41.01431896485528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab989e5fae399%3A0x34f0847eed8b22e0!2zRmF0aWgvxLBzdGFuYnVs!5e0!3m2!1sen!2str!4v1676812409207!5m2!1sen!2str" 
+
+loading="lazy" 
+
+></iframe>
+
+
+
   </div>
   )
 }
