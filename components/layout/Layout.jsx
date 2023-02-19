f4e5a410969f52fe45ca8096d85bfc79785177ca
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-02-19 14:02:02 +0300

    addddd

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -19,7 +19,7 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>Monst - NextJs Tailwind CSS Landing Page</title>
+                <title>Tulin Techmology Site</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
                     rel="preconnect"
