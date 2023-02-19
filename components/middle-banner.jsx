cf3b56633080d231b7f369f13e0bf990bce5a95a
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-02-20 00:19:22 +0300

    add middle banner

---
+++ components/middle-banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    
+    <section className="py-20 bg-blue-500 bg-md-banner">
+    <div className="container">
+        <div className="text-center max-w-xl mx-auto">
+            <h2 className="mb-4 text-3xl lg:text-3xl text-white font-bold font-heading">
+                <span>
+            من نحن و أهم عوامل نجاح شركة تولين تكنولوجي
+            
+                </span>
+               
+                 <br />
+                <span className=' text-xl mt-2'>
+
+                رؤيتنا المستقبلية تضمن لك تحقيق النجاح المستمر و الوصول إلى عملاء أكثر فعالية و نجاح مضمون و تحقيق مبيعات مستمرة حيث إختيار الجمهور المناسب و المكان المناسب و بأقل تكلفة ممكنة 
+                </span>
+            </h2>
+        
+           
+        </div>
+    </div>
+</section>
+  )
+}
