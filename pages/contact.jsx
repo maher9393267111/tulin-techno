88a2790187d6d81d0fe6ad8da04f073155165935
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-02-19 15:44:38 +0300

    add map to contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/layout/Layout';
 import TextEffect from "../components/elements/TextEffect"
 import Link from "next/link";
+import MapBox from '../components/mapbox';
 
 const Contact = () => {
     return (
@@ -55,6 +56,53 @@ const Contact = () => {
                                     We will <span className="text-blue-500">be glad</span> to hear from you!
                                 </h2>
                             </div>
+
+
+{/* // Details to call us  */}
+
+
+<div className="flex flex-wrap -mx-3 text-center">
+                            <div className="w-1/2 lg:w-1/3 px-3 mb-12 wow animate__animated animate__fadeIn animated" data-wow-delay=".1s">
+                                <svg className="mb-6 h-8 w-8 mx-auto text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
+                                </svg>
+                                <div className="leading-relaxed">
+                                    <span className="text-sm text-blueGray-400">Phone</span>
+                                    <p>+ 48 654-430-309</p>
+                                    <p>+ 1 6532-430-309</p>
+                                </div>
+                            </div>
+                            <div className="w-1/2 lg:w-1/3 px-3 mb-12 wow animate__animated animate__fadeIn animated" data-wow-delay=".3s">
+                                <svg className="mb-6 h-8 w-8 mx-auto text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                                </svg>
+                                <div className="leading-relaxed">
+                                    <span className="text-sm text-blueGray-400">E-mail</span>
+                                    <p>contact@monst.com</p>
+                                    <p>pat@example.com</p>
+                                </div>
+                            </div>
+                            <div className="w-full lg:w-1/3 px-3 mb-12 wow animate__animated animate__fadeIn animated" data-wow-delay=".5s">
+                                <svg className="mb-6 h-8 w-8 mx-auto text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                </svg>
+                                <div className="leading-relaxed">
+                                    <span className="text-sm text-blueGray-400">Address</span>
+                                    <p>11567 E Broadview Dr</p>
+                                    <p>Colorado(CO), 80117</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+{/* end call us  */}
+
+
+
+
+
                             <div>
                                 <form>
                                   
@@ -90,6 +138,27 @@ const Contact = () => {
                         </div>
                     </div>
                 </section>
+
+
+ {/* MAP here   */}
+
+<div className=' container my-4'>
+
+
+ <section className="map-section">
+        <div className="map-outer">
+          <MapBox />
+        </div>
+      </section>
+
+
+
+      </div>
+
+{/* MAP END   */}
+
+
+
 
                 <section className="py-20 bg-blue-500">
                     <div className="container">
