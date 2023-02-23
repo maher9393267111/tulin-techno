324c8cc7ed560280fdac8cdf3c113d49fbb76c48
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-02-23 21:34:39 +0300

    udpate whatsapp message

--- data/social-links.js
+++ data/social-links.js
@@ -5,6 +5,6 @@ export const hrefLinkPhone = 'tel:+' + companyLadaPhone;
 export const companyEmail = 'info@tuline.tech';
 export const hrefLinkEmail = 'mailto:' + companyEmail;
 
-const waMessage = "Hello!%20I%20want%20to%20get%20info%20about%20Golden%20movers'%20services";
+const waMessage = "Hello!%20I%20want%20to%20get%20info%20about%20Tuline%20technology'%20services";
 export const whatsappLink = "https://wa.me/" + companyLadaPhone + "?text=" + waMessage;
 export const whatsapp = <a href={whatsappLink} target="_blank"></a>
