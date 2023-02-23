0f2d104bc32a05b29299e23629a95229fb4dbbdc
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-02-23 14:30:56 +0300

    add office place in amp

--- components/mapbox.jsx
+++ components/mapbox.jsx
@@ -6,11 +6,7 @@ export default function mapbox() {
 
 
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48167.997665822826!2d28.918601990244273!3d41.01431896485528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab989e5fae399%3A0x34f0847eed8b22e0!2zRmF0aWgvxLBzdGFuYnVs!5e0!3m2!1sen!2str!4v1676812409207!5m2!1sen!2str" 
-
-loading="lazy" 
-
-></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3010.1169009392206!2d28.93325181533691!3d41.02269832648633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caba3dd6187817%3A0xd90b4ee95d890ef3!2zS2FyYWfDvG1yw7xrLCBBcnBhIEVtaW5pIEvDtnByw7xzw7wgU2suIDRBLCAzNDA5MSBGYXRpaC_EsHN0YW5idWw!5e0!3m2!1sen!2str!4v1677151787954!5m2!1sen!2str"   loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
 
